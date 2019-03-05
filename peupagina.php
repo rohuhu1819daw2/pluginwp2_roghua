57b972c96214916e44a0f4d4c1977b76c6c0d6f7
root <root@DebianNube.lwc5qrycqhoupl3elytmix32hd.ax.internal.cloudapp.net>
2019-03-05 10:49:13 +0000

    Commit 3

--- peupagina.php
+++ peupagina.php
@@ -9,13 +9,18 @@ Author URI: https://github.com/rohuhu1819daw2
 License: GNU GPLv3
 */
 
+
 function admin_menu_peu_pagina(){
-        $titolPagina="Peu de pagina per defecte";
-        $titolEntrada="Peu de pagina per defecte";
-        $role="administrator";
-        $id="peu_pagina_per_defecte";
-    add_menu_page($titolPagina,$titolEntrada,$role,$id);
-}
-add_action('admin_menu','admin_menu_peu_pagina');
+    $titolPagina="Peu de pagina per defecte";
+    $titolEntrada="Peu de pagina per defecte";
+    $role="administrator";
+    $id="peu_pagina_per_defecte";
+    $nom_funcio="nouPeuPagDef";
+    add_menu_page($titolPagina,$titolEntrada,$role,$id, $nom_funcio);
+    }
+
+function nouPeuPagDef(){
+        include_once('formPeuPag.php');
+    }
 
 ?>
