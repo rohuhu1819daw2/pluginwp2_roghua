23ff5748bc927e9b29a38450486d28f30be6a0a5
root <root@DebianNube.lwc5qrycqhoupl3elytmix32hd.ax.internal.cloudapp.net>
2019-03-05 10:09:31 +0000

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades
+Plugin URI: https://github.com/rohuhu1819daw2/pluginwp2_roghua.git
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Roger Huanuco
+Author URI: https://github.com/rohuhu1819daw2
+License: GNU GPLv3
+*/
+?>
