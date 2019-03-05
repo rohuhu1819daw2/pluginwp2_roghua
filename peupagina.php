58213acef330e74d8fd6e06c3d631af325f5cce3
root <root@DebianNube.lwc5qrycqhoupl3elytmix32hd.ax.internal.cloudapp.net>
2019-03-05 10:39:34 +0000

    Commit 2

--- peupagina.php
+++ peupagina.php
@@ -8,4 +8,14 @@ Author: Roger Huanuco
 Author URI: https://github.com/rohuhu1819daw2
 License: GNU GPLv3
 */
+
+function admin_menu_peu_pagina(){
+        $titolPagina="Peu de pagina per defecte";
+        $titolEntrada="Peu de pagina per defecte";
+        $role="administrator";
+        $id="peu_pagina_per_defecte";
+    add_menu_page($titolPagina,$titolEntrada,$role,$id);
+}
+add_action('admin_menu','admin_menu_peu_pagina');
+
 ?>
